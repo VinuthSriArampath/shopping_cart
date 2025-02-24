8dbd006ad48d611f7239f35c94a97c9461fc1ade
vinuth <vinuthsriarampath@outlook.com>
2025-02-24 13:45:36 +0530

    feature : implement image related crud

---
+++ src/main/java/edu/vinu/dto/ImageDto.java
@@ -0,0 +1,14 @@
+package edu.vinu.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageDto {
+    private Long imageId;
+    private String imageName;
+    private String downloadUrl;
+}
---
+++ src/main/java/edu/vinu/service/image/ImageService.java
@@ -0,0 +1,14 @@
+package edu.vinu.service.image;
+
+import edu.vinu.dto.ImageDto;
+import edu.vinu.model.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface ImageService {
+    Image getImageById(Long id);
+    void deleteImageById(Long id);
+    List<ImageDto> saveImage(List<MultipartFile> files, Long productId);
+    void updateImage(MultipartFile file,Long productId);
+}
