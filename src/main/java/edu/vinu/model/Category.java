49938a3e3689c279d09a0243035952928ada62bf
vinuth <vinuthsriarampath@outlook.com>
2025-02-27 12:47:09 +0530

    fix : decorate products attribute from @JsonIgnore

--- src/main/java/edu/vinu/model/Category.java
+++ src/main/java/edu/vinu/model/Category.java
@@ -1,5 +1,6 @@
 package edu.vinu.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +17,7 @@ public class Category {
     private Long id;
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "category")
     private List<Product> products;
 
