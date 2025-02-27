bc53680dec28be17a71f0728f352635e844bd900
vinuth <vinuthsriarampath@outlook.com>
2025-02-27 13:34:36 +0530

    feature : Implement CartRepository and CartItemRepository

---
+++ src/main/java/edu/vinu/repository/cart_item/CartItemRepository.java
@@ -0,0 +1,8 @@
+package edu.vinu.repository.cart_item;
+
+import edu.vinu.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+    void deleteAllByCartId(Long id);
+}
