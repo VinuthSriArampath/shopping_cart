7fd9ce2eaeb33601480e02acc762c22d5dd79c7d
vinuth <vinuthsriarampath@outlook.com>
2025-02-27 07:17:38 +0530

    chore : remove unused ProductNotFoundException.java

--- src/main/java/edu/vinu/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package edu.vinu.exception;
-
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
