e3b22151009cfe96ff0829eb2c739eb1e1cdcdd3
vinuth <vinuthsriarampath@outlook.com>
2025-02-27 15:44:07 +0530

    feature : add addItem , removeItem and updateTotalAmount methods

--- src/main/java/edu/vinu/model/Cart.java
+++ src/main/java/edu/vinu/model/Cart.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Set;
 @Data
 @AllArgsConstructor
@@ -18,5 +19,27 @@ public class Cart {
     private BigDecimal totalAmount = BigDecimal.ZERO;
 
     @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<CartItem> cartItems;
+    private Set<CartItem> cartItems= new HashSet<>();
+
+    public void addItem(CartItem item) {
+        this.cartItems.add(item);
+        item.setCart(this);
+        updateTotalAmount();
+    }
+
+    public void removeItem(CartItem item) {
+        this.cartItems.remove(item);
+        item.setCart(null);
+        updateTotalAmount();
+    }
+
+    private void updateTotalAmount() {
+        this.totalAmount = cartItems.stream().map(item -> {
+            BigDecimal unitPrice = item.getUnitPrice();
+            if (unitPrice == null) {
+                return  BigDecimal.ZERO;
+            }
+            return unitPrice.multiply(BigDecimal.valueOf(item.getQuantity()));
+        }).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 }
