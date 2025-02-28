1481138f73557af34f9a986f62a8a0d80ed266fa
vinuth <vinuthsriarampath@outlook.com>
2025-02-28 16:17:32 +0530

    feature : implement order Item

---
+++ src/main/java/edu/vinu/model/OrderItem.java
@@ -0,0 +1,38 @@
+package edu.vinu.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public OrderItem(Order order, Product product, int quantity, BigDecimal price) {
+        this.order = order;
+        this.quantity = quantity;
+        this.price = price;
+        this.product = product;
+    }
+}
