9e604a3e94c8471038b98334d564050572cfe928
vinuth <vinuthsriarampath@outlook.com>
2025-02-28 16:15:34 +0530

    feature : implement UpdateUserRequest and CreateUserRequest

---
+++ src/main/java/edu/vinu/request/CreateUserRequest.java
@@ -0,0 +1,10 @@
+package edu.vinu.request;
+
+import lombok.Data;
+@Data
+public class CreateUserRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
