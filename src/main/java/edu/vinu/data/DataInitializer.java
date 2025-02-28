b021d22eae930f949b3d2e530a969807985320b7
vinuth <vinuthsriarampath@outlook.com>
2025-02-28 16:16:33 +0530

    development : implement a DataInitializer to feed database for the first time

---
+++ src/main/java/edu/vinu/data/DataInitializer.java
@@ -0,0 +1,37 @@
+package edu.vinu.data;
+
+import edu.vinu.model.User;
+import edu.vinu.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
+    private final UserRepository userRepository;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        createDefaultUserIfNotExits();
+    }
+
+    private void createDefaultUserIfNotExits(){
+        for (int i = 1; i<=5; i++){
+            String defaultEmail = "user"+i+"@email.com";
+            if (userRepository.existsByEmail(defaultEmail)){
+                continue;
+            }
+            User user = new User();
+            user.setFirstName("The User");
+            user.setLastName("User" + i);
+            user.setEmail(defaultEmail);
+            user.setPassword("123456");
+            userRepository.save(user);
+            log.info("Default vet user " + i + " created successfully.");
+        }
+    }
+}
